42063f1a43cefe1a2b17c4055a285c419d4bc195
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2025-10-17 15:58:38 +0100

    version 0.1.1

--- autoCLI_config/__init__.py
+++ autoCLI_config/__init__.py
@@ -35,7 +35,7 @@ Example:
     >>>     print(f"Training with lr={learning_rate}, batch_size={batch_size}")
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Ruben Sanchez-Garcia"
 __email__ = "rsanchezgarcia@faculty.ie.edu"
 
